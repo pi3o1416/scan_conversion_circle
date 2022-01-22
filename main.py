425b7e541bfb64149c0e42f1c5f73baeb0d4085f
monir hossain <mmonir.m.29@gmail.com>
2022-01-22 15:41:58 +0600

    radius input option

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ from OpenGL import GL
 from scan_conversion_circle import mid_point_circle, circle_draw 
 
 def main():
-
+    r = int(input("Redius :"))
 #    vertices = circle_draw(r=1, density=10)
     vertices = mid_point_circle(r=200)
 
-    window = Window(720, 720, 400, 200)
+    window = Window(480, 480, 400, 200)
     window.set_color(0, 0.1, 0.1, 1)
     window.set_vertices(vertices)
     window.main_loop(GL.GL_POINTS)
